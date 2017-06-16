5f3f5b0f985e5e746799c4778f5cfbad8ce2c5ed
FlyingBoar <montecristo310@gmail.com>
2017-06-16 15:06:11 +0200

    - Implementata l'istanziazione di un Item all'interno della room una volta trascinato. - bozza per generazione di item nell'inventario ( non completato )

---
+++ Assets/Scripts/UI/InventoryController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class InventoryController : MonoBehaviour {
+
+    List<Slot> slots = new List<Slot>();
+
+	// Use this for initialization
+	void Start () {
+        slots = GetComponentsInChildren<Slot>().ToList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
