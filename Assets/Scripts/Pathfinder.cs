c66527ed9f1161da9114853b6a6ac3977fb463ba
AlbertoDesktop <alberto.paparella.88@gmail.com>
2017-06-18 14:59:34 +0200

    progressione parziale

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -26,12 +26,41 @@ namespace Framework.Pathfinding
         {
             List<INetworkable> foundPath = new List<INetworkable>();
 
+            //------Format to set Actual as defoult of this method's _start parameter
+            if (_start == null)
+                foundPath.Add(Actual);
+            else
+                foundPath.Add(_start);
+
+            List<PathStep> possiblePaths = new List<PathStep>() { new PathStep(foundPath[0], foundPath[0], _target) };
+            //-----------------------------------------------------------------------
+            FindPath(_target, possiblePaths);
+
             return foundPath;
         }
 
         List<PathStep> FindPath(INetworkable _target, List<PathStep> _givenPath)
         {
-            return null;
+            List<PathStep> outcome = _givenPath;
+            float pathDistance = -1;
+            foreach (PathStep step in _givenPath)
+            {
+                foreach (INetworkable closeNode in step.node.Links)
+                {
+                    if (pathDistance == -1)
+                    {
+                        outcome.Add(new PathStep(closeNode, _givenPath[0].node, _target));
+                        pathDistance = outcome[outcome.Count - 1].distance;
+                    }
+                    else if (outcome[outcome.Count - 1].distance < pathDistance)
+                    {
+                        outcome.Remove(outcome[outcome.Count - 1]);
+                        outcome.Add(new PathStep(closeNode, _givenPath[0].node, _target));
+                        pathDistance = outcome[outcome.Count - 1].distance;
+                    }
+                }
+            }
+            return outcome;
         }
 
         List<INetworkable> PathStepToINetworkableList( List<PathStep> _PathSteps)
@@ -50,19 +79,21 @@ namespace Framework.Pathfinding
         class PathStep
         {
             public INetworkable node;
+            public INetworkable targetNode { get; private set; }
+            public INetworkable startNode { get; private set; }
             public float distance
             {
                 get { return originOffSet + targetOffSet; }
             }
-            public float originOffSet;
-            public float targetOffSet;
+            public float originOffSet { get { return Vector3.Distance(node.spacePosition, startNode.spacePosition); } }
+            public float targetOffSet { get { return Vector3.Distance(node.spacePosition, targetNode.spacePosition); } }
 
             PathStep() { }
-            public PathStep(INetworkable _node, float _originOffSet, float _targetOffSet)
+            public PathStep(INetworkable _node, INetworkable _startNode, INetworkable _targetNoide)
             {
                 node = _node;
-                originOffSet = _originOffSet;
-                targetOffSet = _targetOffSet;
+                startNode = _startNode;
+                targetNode = _targetNoide;
             }
         }
     }
