e831cd949b15951f05587f7ca9a3d079b1156626
AlbeGame <alberto.paparella.88@gmail.com>
2017-06-21 17:36:54 +0200

    early version of AI

--- Assets/Scripts/Dumby.cs
+++ Assets/Scripts/Dumby.cs
@@ -25,45 +25,17 @@ public class Dumby : MonoBehaviour {
     public float interactionRadius = 0.1f;
 
 	// Use this for initialization
-	public void Init () {
+	public void Setup () {
         grid = FindObjectOfType<GridController>();
         pathfinder = new Pathfinder(currentNode);
         animator =  GetComponentInChildren<Animator>();
-	}	
-	
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Keypad1))
-           path = pathfinder.FindPath(grid.GetSpecificGridNode(new GridPosition(0, 0)), currentNode);
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            animState = AnimationState.Running;
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-            animState = AnimationState.Idle;
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            animState = AnimationState.Fallen;
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-            animState = AnimationState.Fighting;
-
-        if (path != null)
-            WalkTroughtNodes(path);
 	}
-    
-    void WalkTroughtNodes(List<INetworkable> _path)
+
+    private void Update()
     {
-        INetworkable actualObjective = _path[0];
-
-        transform.LookAt(actualObjective.spacePosition);
-        transform.position = actualObjective.spacePosition;
-
-        if (Vector3.Distance(currentNode.WorldPosition, actualObjective.spacePosition) <= interactionRadius)
-        {
-            if (_path.Count == 1)
-                _path = new List<INetworkable>();
-            else
-                _path.Remove(_path[0]);
-        }
+            
     }
-
+    
     public enum AnimationState
     {
         Fallen = 0,
