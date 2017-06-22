7f1b8db108b5fd5bc2cdc626a770ded7be5fe19a
AlbertoDesktop <alberto.paparella.88@gmail.com>
2017-06-22 20:40:55 +0200

    Inserimento iniziale struttura di Items

---
+++ Assets/Scripts/ItemAlb/ItemGeneric.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DumbProject.Items {
+    public class ItemGeneric : MonoBehaviour
+    {
+        void Setup() { }
+        void Init() { }
+    }
+}
