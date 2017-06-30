1ff500b4c66aa28a177f6c8c7fcf8e097a33cbd7
DESKTOP-B9NGNNA\Eh_student <alberto.paparella.88@gmail.com>
2017-06-30 12:50:12 +0200

    Piccole modifiche nel tentativo di estromettere il push di LUCA

--- Assets/Scripts/AI/Actions/EvaluateNextObjective.cs
+++ Assets/Scripts/AI/Actions/EvaluateNextObjective.cs
@@ -9,7 +9,12 @@ namespace DumbProject.AI
     [CreateAssetMenu(menuName = "AI/State/Action/EvaluateNextObjective")]
     public class EvaluateNextObjective : Action
     {
-        INetworkable objectiveNode { get { return Generic.GameManager.I.TestNode; } }
+        public IDroppable IdroppableToInteract;
+
+        INetworkable objectiveNode { get {
+                return Generic.GameManager.I.TestNode;
+
+            } }
         Pathfinder pathfinder;
 
         public override void Act(AIController _controller)
@@ -24,6 +29,14 @@ namespace DumbProject.AI
 
             if((_controller.nodePath == null || _controller.nodePath.Count == 0) && _controller.CurrentNode != objectiveNode)
                 _controller.nodePath = pathfinder.FindPath(objectiveNode);
+        }
+
+        INetworkable ChooseBetweenPossibilities()
+        {
+            if (IdroppableToInteract != null)
+                return Generic.GameManager.I.MainGridCtrl.GetSpecificGridNode(Vector3.zero);
+
+            return Generic.GameManager.I.TestNode;
         }
     }
 }
