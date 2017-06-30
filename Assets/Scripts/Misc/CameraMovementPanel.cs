470dedd85e7e76daab977a54470b56aa94ca3f6b
FlyingBoar <montecristo310@gmai.com>
2017-06-30 17:11:51 +0200

    - Aggiunta rotazione della UI con il tasto "X" con alcuni Bug relativi al movimento delle stanze in modalità orizzontale

--- Assets/Scripts/Misc/CameraMovementPanel.cs
+++ Assets/Scripts/Misc/CameraMovementPanel.cs
@@ -9,10 +9,22 @@ namespace DumbProject
 {
     public class CameraMovementPanel : MonoBehaviour, IDragHandler
     {
+
         public void OnDrag(PointerEventData eventData)
         {
             Vector2 direction = (eventData.position - eventData.pressPosition).normalized;
             GameManager.I.CameraController.MoveCamera(-direction);
+        }
+
+        private void Update()
+        {
+            Touch[] touchs = Input.touches;
+
+            if(touchs.Length > 1)
+            {
+                //Vector2 direction = (touchs[1].position - touchs[1].
+            }
+
         }
     }
 }
