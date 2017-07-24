5dc26b17013bc85231b10a3165953e773875632f
AlbeGame <alberto.paparella.88@gmail.com>
2017-07-24 11:37:14 +0200

    Introdotto CameraController (attenzione a non prendere quello di TextMeshPro. Usare quello in Dumbproject.Generic)

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DumbProject.Generic
+{
+    public class CameraController : MonoBehaviour {
+
+        public float TranslationSpeed = 10;
+        public float ZoomSpeed = 10;
+
+        public float ZoomMax = 10;
+        public float ZoomMin = 10;
+        float currentZoom;
+
+        Vector3 startingPosition;
+        private void Awake()
+        {
+            startingPosition = transform.position;
+        }
+
+        public void Translate(Vector3 _objectivePosition)
+        {
+            float x = Mathf.Lerp(transform.position.x, _objectivePosition.x, TranslationSpeed);
+            float y = Mathf.Lerp(transform.position.y, _objectivePosition.y, TranslationSpeed);
+            float z = Mathf.Lerp(transform.position.y, _objectivePosition.y, TranslationSpeed);
+
+            Vector3 headingPoint = new Vector3(x, y, z);
+            transform.position = headingPoint;
+        }
+
+        public void Zoom(float _zoomDelta)
+        {
+            if (currentZoom != _zoomDelta)
+            {
+                currentZoom += Mathf.Lerp(currentZoom, _zoomDelta, ZoomSpeed);
+                if (currentZoom < ZoomMin)
+                    currentZoom = ZoomMin;
+                if (currentZoom > ZoomMax)
+                    currentZoom = ZoomMax;
+
+                Translate(startingPosition + Vector3.forward * currentZoom);
+            }
+
+        }
+    }
+}
