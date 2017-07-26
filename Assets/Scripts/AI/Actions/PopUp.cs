e2b69dc57e9286d5f5ce1930a94fa98a00bbb713
AlbeGame <alberto.paparella.88@gmail.com>
2017-07-26 11:26:32 +0200

    Inserito PopUp come action della AI

---
+++ Assets/Scripts/AI/Actions/PopUp.cs
@@ -0,0 +1,52 @@
+﻿using DG.Tweening;
+using UnityEngine.UI;
+using Framework.AI;
+using UnityEngine;
+using DumbProject.Generic;
+
+namespace DumbProject.AI {
+    [CreateAssetMenu(menuName = "AI/State/Action/PopUp")]
+    public class PopUp : Action
+    {
+        public Sprite Icon;
+        public float VerticalOffSet;
+        public float VerticalJumpHeight;
+        public float DisplayTime;
+
+        GameObject actualPopUp;
+        SpriteRenderer popUpRenderer;
+
+        public override void Act(AIController _controller)
+        {
+            AIActor actor = _controller as AIActor;
+
+            PopUpSetup(actor);
+            ShowPopUp(actor);
+        }
+
+        void ShowPopUp(AIActor _actor)
+        {
+            actualPopUp.transform.DOJump(actualPopUp.transform.position, VerticalJumpHeight, 1, DisplayTime / 2)
+                .SetDelay(DisplayTime/2)
+                .OnComplete(()=> { actualPopUp.SetActive(false); });
+        }
+
+        void PopUpSetup(AIActor _actor)
+        {
+            if (actualPopUp == null)
+                actualPopUp = new GameObject("PopUp");
+            else
+                actualPopUp.SetActive(true);
+
+            if (popUpRenderer == null)
+                popUpRenderer = actualPopUp.AddComponent<SpriteRenderer>();
+            else
+                popUpRenderer.enabled = true;
+
+            popUpRenderer.sprite = Icon;
+            actualPopUp.transform.parent = _actor.transform;
+            actualPopUp.transform.position = _actor.transform.position + new Vector3(0, VerticalOffSet,0);
+
+        }
+    }
+}
