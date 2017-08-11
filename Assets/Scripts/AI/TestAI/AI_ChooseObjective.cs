de4c68e9b9f90bb9d40c3e63b4af14d91838c3b2
AlbeGame <alberto.paparella.88@gmail.com>
2017-08-11 12:24:39 +0200

    Inserita la scelta del obiettivo. Resta da assemblare e testare la  AI

--- Assets/Scripts/AI/TestAI/AI_ChooseObjective.cs
+++ Assets/Scripts/AI/TestAI/AI_ChooseObjective.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using DumbProject.Rooms;
+using DumbProject.Generic;
+using DumbProject.Items;
+using Framework.Pathfinding;
 using UnityEngine;
 
 namespace Framework.AI
@@ -8,9 +10,72 @@ namespace Framework.AI
     [CreateAssetMenu(menuName = "AI/State/NewAction/ChooseObjective")]
     public class AI_ChooseObjective : AI_Action
     {
+        public List<GenericType> PriorityList = new List<GenericType>();
+
         protected override bool Act(AI_Controller _controller)
         {
-            return true;
+            Room room = (_controller as Dumby).CurrentRoom;
+            IInteractable objective = SearchInRoom(room);
+
+            if (objective == null)
+                objective = SearchInDungeon((_controller as Dumby).CurrentNetworkable);
+
+            if (objective != null)
+            {
+                (_controller as Dumby).Objective = GameManager.I.MainGridCtrl.GetSpecificNetNode(objective.Transf.position);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        IInteractable SearchInRoom(Room _room)
+        {
+            foreach (GenericType priorityType in PriorityList)
+            {
+                foreach (IInteractable avaibleItem in _room.InteractableAvailable)
+                {
+                    ItemGeneric item = (avaibleItem as MonoBehaviour).GetComponent<ItemGeneric>();
+                    if(item != null)
+                    {
+                        if (item.Data.Type == priorityType)
+                            return avaibleItem;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        IInteractable SearchInDungeon(INetworkable _actualNode)
+        {
+            List<Door> doorsToExplore = GameManager.I.DungeonMng.GetAllUnexploredDoors();
+            int distancesInNodes = GameManager.I.MainGridCtrl.GridHeight * GameManager.I.MainGridCtrl.GridWidth;
+            IInteractable closestDoor = null;
+
+            if (doorsToExplore.Count <= 0)
+                return null;
+
+            foreach (Door door in doorsToExplore)
+            {
+                int dis = Pathfinder.FindPath(door.RelativeNetNode, _actualNode).Count;
+                if(closestDoor == null)
+                {
+                    distancesInNodes = dis;
+                    closestDoor = door;
+                }
+                else
+                {
+                    if(dis < distancesInNodes)
+                    {
+                        distancesInNodes = dis;
+                        closestDoor = door;
+                    }
+                }
+            }
+
+            return closestDoor;
         }
     }
 }
--- Assets/Scripts/AI/TestAI/Dumby.cs
+++ Assets/Scripts/AI/TestAI/Dumby.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using DumbProject.Rooms;
 using Framework.AI;
 using Framework.Pathfinding;
 using UnityEngine;
@@ -7,9 +7,36 @@ namespace DumbProject.Generic
 {
     public class Dumby : AI_Controller, IPathfinder
     {
+        private Room _currentRoom;
+        public Room CurrentRoom
+        {
+            get
+            {
+                if (_currentRoom == null)
+                    _currentRoom = GameManager.I.MainGridCtrl.GetSpecificGridNode(transform.position).RelativeCell.RelativeRoom;
+                return _currentRoom;
+            }
+            protected set { _currentRoom = value; }
+        }
+
         #region IPathfinder
         public INetworkable Objective { get; set; }
-        public INetworkable CurrentNetworkable { get; set; }
+        private INetworkable _currentNetworkable;
+        public INetworkable CurrentNetworkable
+        {
+            get
+            {
+                if (_currentNetworkable == null)
+                    _currentNetworkable = GameManager.I.MainGridCtrl.GetSpecificNetNode(transform.position);
+                return _currentNetworkable;
+            }
+            set
+            {
+                if (GameManager.I.MainGridCtrl.GetSpecificNetNode(value.spacePosition) == null)
+                    CurrentRoom = GameManager.I.MainGridCtrl.GetSpecificGridNode(value.spacePosition).RelativeCell.RelativeRoom;
+                _currentNetworkable = value;
+            }
+        }
 
         public INetworkable[] Path { get; set; }
         #endregion
