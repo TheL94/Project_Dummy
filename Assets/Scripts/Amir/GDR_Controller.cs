c2bf744565f265c93d0474d125bb5e7263886bba
AlienWare <amir.basyouni@gmail.com>
2017-08-18 17:05:02 +0200

    GDD pg 15 e 16. GDR_Controller da testare

--- Assets/Scripts/Amir/GDR_Controller.cs
+++ Assets/Scripts/Amir/GDR_Controller.cs
@@ -1,68 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DumbProject.Grid;
+using DumbProject.Generic;
+using DumbProject.Items;
 
-public class GDR_Controller : MonoBehaviour {
-
-
-    private float experienceCounter;
-    public float ExperienceCounter
+namespace DumbProject.Rooms
+{
+    public class GDR_Controller : MonoBehaviour
     {
-        get { return experienceCounter; }
-        set { experienceCounter = value; }
-    }
-
-    private int playerLevel;
-
-    public int PlayerLevel
-    {
-        get { return playerLevel; }
-        set { playerLevel = value; }
-    }
-
-
-    /// <summary>
-    /// Check the experience fort the next Player Level
-    /// </summary>
-    public void  CheckExperience()
-    {
-        switch ((int)ExperienceCounter)
+        #region Variables
+        private float experienceCounter;
+        public float ExperienceCounter
         {
-            case 10:
-                PlayerLevel++;
-                ExperienceCounter = 0;
-                break;
-            case 20:
-                PlayerLevel++;
-                ExperienceCounter = 0;
-                break;
-            case 30:
-                PlayerLevel++;
-                ExperienceCounter = 0;
-                break;
-            default:
-                break;
+            get { return experienceCounter; }
+            set
+            {
+                experienceCounter = value;
+                CheckExperience();
+            }
         }
-    }
 
-    public void SetPlayerLevel()
-    {
-        switch (PlayerLevel)
+        public int PlayerLevel;
+
+        public float ExperienceForNextLevel;
+        #endregion
+
+        #region API
+        /// <summary>
+        /// Check the experience for the next Player Level
+        /// </summary>
+        public void CheckExperience()
         {
-            case 1:
-                //Dumby.Speed = Dumby.Speed * (1,05);
-                //Dumby.Attacck = Dumby.Attack +0.25:
-                break;
-            case 2:
-                //Dumby.Speed = Dumby.Speed * (1,05);
-                //Dumby.Attacck = Dumby.Attack +0.25:
-                break;
-            case 3:
-                //Dumby.Speed = Dumby.Speed * (1,05);
-                //Dumby.Attacck = Dumby.Attack +0.25:
-                break;
-            default:
-                break;
+            if (ExperienceCounter >= ExperienceForNextLevel)
+            {
+                PlayerLevel++;
+                ExperienceCounter = ExperienceCounter - ExperienceForNextLevel;
+                ExperienceForNextLevel = ExperienceForNextLevel * 1.2f;
+                ModifyPlayerStats();
+            }
         }
+        /// <summary>
+        /// Modify the Player statistic each time he reach a new level.
+        /// </summary>
+        public void ModifyPlayerStats()
+        {
+            //Dumby.Speed = Dumby.Speed * 1,05f;
+            //Dumby.Attack = Dumby.Attack +0.25f:
+        }
+
+        #region GetExperience
+        /// <summary>
+        /// Get the experience from the Enemy
+        /// </summary>
+        public void GetEnemyExperience(/*Enemy enemy*/)
+        {
+
+            //if (Enemy != null)
+            //{
+            //    ExperienceCounter += enemy.experience;
+            //}
+
+        }
+
+        /// <summary>
+        /// Get the experience from the Obstacles
+        /// </summary>
+        /// <param name="_obstacle"></param>
+        public void GetObstacleExperience(Obstacle _obstacle)
+        {
+            switch (_obstacle)
+            {
+                case Obstacle.BananaPeel:
+                    experienceCounter += 0.5f;
+                    break;
+                case Obstacle.Kitties:
+                    experienceCounter += 0.75f;
+                    break;
+                case Obstacle.ShirtPresss:
+                    experienceCounter += 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Get the Experience when the room status id Explored.
+        /// </summary>
+        /// <param name="_room"></param>
+        public void GetExperienceFromNewRoom(Room _room)
+        {
+            if (_room != null)
+            {
+                if (_room.StatusOfExploration == ExplorationStatus.Explored)
+                {
+                    /*ExperienceCounter++*/
+                }
+            }
+        }
+    } 
+    #endregion
+    #endregion
+
+    public enum Obstacle
+    {
+        BananaPeel,
+        Kitties,
+        ShirtPresss
     }
 }
