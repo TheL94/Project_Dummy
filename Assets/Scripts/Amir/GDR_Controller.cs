9592079ff9434010dc922d5629d931d515670863
Amir Basyouni <amir.basyouni@gmail.com>
2017-08-22 12:42:40 +0200

    https://docs.google.com/document/d/10rkYDSK3_3G_ytNO8eqLocU2ti8oYuJHiQE1lpCLkoA/edit DONE!

--- Assets/Scripts/Amir/GDR_Controller.cs
+++ Assets/Scripts/Amir/GDR_Controller.cs
@@ -10,6 +10,9 @@ namespace DumbProject.Rooms
     public class GDR_Controller : MonoBehaviour
     {
         #region Variables
+
+        public int PlayerLevel;
+
         private float experienceCounter;
         public float ExperienceCounter
         {
@@ -17,49 +20,125 @@ namespace DumbProject.Rooms
             set
             {
                 experienceCounter = value;
+                GetExperienceMultiplier();
                 CheckExperience();
             }
         }
 
-        public int PlayerLevel;
-
         public float ExperienceForNextLevel;
+
+        public float Life;
+        public float ActualLife;
+        public float Speed;
+        public float Attack;
+
+
+        private float experienceToGive;
+        public float ExperienceToGive
+        {
+            get { return experienceToGive; }
+            set
+            {
+                experienceToGive = value;
+                GetExperienceMultiplier();
+            }
+        }
+        public float ExperienceMuliplier;
+
+        GDR_Controller target;
+        public ExperienceType experienceType;
+        public Obstacle obstacleType;
         #endregion
 
         #region API
         /// <summary>
         /// Check the experience for the next Player Level
         /// </summary>
-        public void CheckExperience()
+        public bool CheckExperience()
         {
+            float tempExperienceForlastLevel = ExperienceForNextLevel / 1.2f;
             if (ExperienceCounter >= ExperienceForNextLevel)
             {
                 PlayerLevel++;
                 ExperienceCounter = ExperienceCounter - ExperienceForNextLevel;
                 ExperienceForNextLevel = ExperienceForNextLevel * 1.2f;
                 ModifyPlayerStats();
+                return true;
             }
+            if (ExperienceCounter < tempExperienceForlastLevel)
+            {
+                PlayerLevel--;
+                ExperienceForNextLevel = tempExperienceForlastLevel;
+                ModifyPlayerStats();
+            }
+            return false;
         }
         /// <summary>
         /// Modify the Player statistic each time he reach a new level.
         /// </summary>
         public void ModifyPlayerStats()
         {
-            //Dumby.Speed = Dumby.Speed * 1,05f;
-            //Dumby.Attack = Dumby.Attack +0.25f:
+            if (CheckExperience() == true)
+            {
+                Speed = Speed * 1.05f;
+                Attack = Attack + 0.25f;
+            }
+            else
+            {
+                Speed = Speed * (-1.05f);
+                Attack = Attack + 0.25f;
+            }
+        }
+
+        /// <summary>
+        /// Get the target and attack it.
+        /// </summary>
+        /// <param name="_target"></param>
+        public void AttackTarget(GDR_Controller _target)
+        {
+            if (target = null)
+            {
+                target = _target;
+            }
+            _target.Life -= Attack;
+        }
+        /// <summary>
+        /// Get the Experience from the Difficulty of the Enemy
+        /// </summary>
+        /// <returns></returns>
+        public float GetExperienceMultiplier()
+        {
+            ExperienceToGive = ExperienceToGive * ExperienceMuliplier;
+            return ExperienceToGive;
         }
 
         #region GetExperience
+
+
         /// <summary>
         /// Get the experience from the Enemy
         /// </summary>
-        public void GetEnemyExperience(/*Enemy enemy*/)
+        public void GetExperience(ExperienceType _experienceType)
         {
-
-            //if (Enemy != null)
-            //{
-            //    ExperienceCounter += enemy.experience;
-            //}
+            Dumby dumby = FindObjectOfType<Dumby>();
+            switch (_experienceType)
+            {
+                case ExperienceType.Enemy:
+                    ExperienceCounter += GetExperienceMultiplier();
+                    if (target != null)
+                    {
+                        target = null;
+                    }
+                    break;
+                case ExperienceType.Obstacle:
+                    GetObstacleExperience(obstacleType);
+                    break;
+                case ExperienceType.Room:
+                    GetExperienceFromNewRoom(dumby.CurrentRoom);
+                    break;
+                default:
+                    break;
+            }
 
         }
 
@@ -67,7 +146,7 @@ namespace DumbProject.Rooms
         /// Get the experience from the Obstacles
         /// </summary>
         /// <param name="_obstacle"></param>
-        public void GetObstacleExperience(Obstacle _obstacle)
+        void GetObstacleExperience(Obstacle _obstacle)
         {
             switch (_obstacle)
             {
@@ -89,19 +168,26 @@ namespace DumbProject.Rooms
         /// Get the Experience when the room status id Explored.
         /// </summary>
         /// <param name="_room"></param>
-        public void GetExperienceFromNewRoom(Room _room)
+        void GetExperienceFromNewRoom(Room _room)
         {
             if (_room != null)
             {
                 if (_room.StatusOfExploration == ExplorationStatus.Explored)
                 {
-                    /*ExperienceCounter++*/
+                    ExperienceCounter++;
                 }
             }
         }
-    } 
+    }
     #endregion
     #endregion
+
+    public enum ExperienceType
+    {
+        Enemy,
+        Obstacle,
+        Room
+    }
 
     public enum Obstacle
     {
