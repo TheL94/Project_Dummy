c00fcafb8aeca26e2f74db971e91bcf2f19c56ac
AlbeGame <alberto.paparella.88@gmail.com>
2017-09-26 12:22:20 +0200

    Camera_Controller

---
+++ Assets/Scripts/Misc/Camera_CinematicController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Camera_CinematicController : MonoBehaviour {
+
+    public float Duration;
+
+	public void Execute(Vector3 _target)
+    {
+        transform.DOMove(new Vector3(_target.x, transform.position.y, _target.z), Duration).SmoothRewind();
+    }
+}
