c29353510f9c2fea6f4d05268ec1055f9910b9a2
AlbeGame <alberto.paparella.88@gmail.com>
2017-09-26 12:55:33 +0200

    Modifiche a Camera_CinematicController

--- Assets/Scripts/Misc/Camera_CinematicController.cs
+++ Assets/Scripts/Misc/Camera_CinematicController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 using DumbProject.Flow;
 
 namespace DumbProject.Generic {
-    public class Camera_CinematicController : MonoBehaviour {
+    public class Camera_CinematicController : MonoBehaviour
+    {
 
         public float Duration;
 
         public void Execute(Vector3 _target)
         {
-            Tween move = transform.DOMove(new Vector3(_target.x, transform.position.y, _target.z), Duration);
-            //move.SmoothRewind();
+            Transform initialTransform = transform;
 
-            move.OnComplete(() => { GameManager.I.ChageFlowState(FlowState.Gameplay);
-                GameManager.I.InputHndl.enabled = false;
+            transform.DOMove(new Vector3(_target.x, initialTransform.position.y, _target.z), Duration / 2).OnComplete(() =>
+            {
+                transform.DOMove(initialTransform.position, Duration / 2).OnComplete(() =>
+                {
+                    GameManager.I.ChageFlowState(FlowState.Gameplay);
+                    GameManager.I.InputHndl.enabled = true;
+                });
             });
         }
     }
