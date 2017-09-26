1e85e739d4cb5f14bebdfe9a140feeebe5ed811b
Luca Garbelli <lu.gbelli@hotmail.it>
2017-09-26 16:31:54 +0200

    - aggiunt trappola, da finire

---
+++ Assets/Scripts/Items/Trap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DumbProject.Items
+{
+    public class Trap : MonoBehaviour
+    {
+        public TrapData TrapValues;
+        public void Init(GenericDroppableData _values)
+        {
+            TrapValues = _values as TrapData;
+            TrapValues.Type = GenericType.Trap;
+        }
+    }
+}
