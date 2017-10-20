0572da0b61c1b055da308dcb21f45dd246947e3f
Alberto <alberto.paparella.88@gmail.com>
2017-10-20 12:03:05 +0200

    Modifica minima al custom button

--- Assets/Scripts/UI/CustomButton.cs
+++ Assets/Scripts/UI/CustomButton.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomButton : MonoBehaviour {
+namespace DumbProject.UI
+{
+    public class CustomButton : MonoBehaviour
+    {
 
-
+    }
 }
