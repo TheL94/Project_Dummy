87bfc63c28c72c9ed2ac6963060cc6364778a18c
Alberto <alberto.paparella.88@gmail.com>
2017-10-23 16:10:59 +0200

    Sistemata la posizione dell'indicatore a schermo

--- Assets/Scripts/Misc/DumbyIndicatorManager.cs
+++ Assets/Scripts/Misc/DumbyIndicatorManager.cs
@@ -77,9 +77,9 @@ namespace DumbProject.UI
         /// </summary>
         void UpdateIndicatorPosition()
         {
-            Vector3 iconPos = DumbyIcon.transform.position;
-            Vector3 iconNewPos = iconPos;
             dumbyPosition = Camera.main.WorldToScreenPoint(transform.position) + Vector3.up * 70;
+            Vector3 iconPos = dumbyPosition;
+            Vector3 iconNewPos = iconPos;
 
             if (CheckIfInsideTheScreen(dumbyPosition))
             {
@@ -87,34 +87,34 @@ namespace DumbProject.UI
             }
             else
             {
-                if (iconPos.x > Screen.width - offset)
-                    if (iconPos.y < UpLeftFrameCorner.y + offset)
+                if (iconPos.x >= Screen.width - offset)
+                    if (iconPos.y < UpLeftFrameCorner.y + offset + float.Epsilon)
                         iconNewPos = new Vector3(Screen.width - offset, UpLeftFrameCorner.y, 0);
-                    else if (iconPos.y > Screen.height - offset)
+                    else if (iconPos.y >= Screen.height - offset)
                         iconNewPos = new Vector3(Screen.width - offset, Screen.height - offset, 0);
                     else
                         iconNewPos = new Vector3(Screen.width - offset, dumbyPosition.y, 0);
 
-                if (iconPos.x < Screen.width - offset)
-                    if (iconPos.y <= UpLeftFrameCorner.y + offset)
+                if (iconPos.x > UpLeftFrameCorner.x - offset && iconPos.x < Screen.width - offset)
+                    if (iconPos.y <= UpLeftFrameCorner.y + offset + float.Epsilon)
                         iconNewPos = new Vector3(dumbyPosition.x, UpLeftFrameCorner.y - offset, 0);
-                    else if (iconPos.y > Screen.height - offset)
+                    else if (iconPos.y >= Screen.height - offset)
                         iconNewPos = new Vector3(dumbyPosition.x, Screen.height - offset, 0);
                     else
                         iconNewPos = new Vector3(dumbyPosition.x, dumbyPosition.y, 0);
 
-                if (iconPos.x <= UpLeftFrameCorner.x - offset)
-                    if (iconPos.y <= offset)
+                if (iconPos.x <= UpLeftFrameCorner.x - offset && iconPos.x >= offset)
+                    if (iconPos.y <= offset + float.Epsilon)
                         iconNewPos = new Vector3(dumbyPosition.x, offset, 0);
-                    else if (iconPos.y > Screen.height - offset)
+                    else if (iconPos.y >= Screen.height - offset)
                         iconNewPos = new Vector3(dumbyPosition.x, Screen.height - offset, 0);
                     else
                         iconNewPos = new Vector3(dumbyPosition.x, dumbyPosition.y, 0);
 
                 if (iconPos.x < offset)
-                    if (iconPos.y < offset)
+                    if (iconPos.y <= offset + float.Epsilon)
                         iconNewPos = new Vector3(offset, offset, 0);
-                    else if (iconPos.y > Screen.height - offset)
+                    else if (iconPos.y >= Screen.height - offset)
                         iconNewPos = new Vector3(offset, Screen.height - offset, 0);
                     else
                         iconNewPos = new Vector3(offset, dumbyPosition.y, 0);
@@ -140,7 +140,7 @@ namespace DumbProject.UI
         {
             Gizmos.color = Color.magenta;
             //Draw della orizzontale
-            Gizmos.DrawLine(UpLeftFrameCorner, new Vector2(Screen.width, UpLeftFrameCorner.y));
+            Gizmos.DrawLine(UpLeftFrameCorner, new Vector2(UpLeftFrameCorner.x, Screen.width));
             //Draw della Verticale
             Gizmos.DrawLine(UpLeftFrameCorner, new Vector2(UpLeftFrameCorner.x, 0));
         }
