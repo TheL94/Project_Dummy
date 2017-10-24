c94fdc7fb51a46bfabc1e33f58dbd16e20f53ce0
Luca Garbelli <lu.gbelli@hotmail.it>
2017-10-24 14:11:23 +0200

    - inizio aggiunta chest

---
+++ Assets/Scripts/Interfaces/I_GDR_EquippableHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DumbProject.GDR_System
+{
+    public interface I_GDR_EquippableHolder
+    {
+
+
+    }
+}
+
