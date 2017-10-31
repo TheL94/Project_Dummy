9d16898bcab7225865e7a8515feb88be20e680df
Alberto <alberto.paparella.88@gmail.com>
2017-10-31 10:51:54 +0100

    aggiunto timer al pulsante del turno

--- Assets/Scripts/Misc/Button_PlayStop.cs
+++ Assets/Scripts/Misc/Button_PlayStop.cs
@@ -8,19 +8,57 @@ namespace DumbProject.Generic {
         Dumby dumbyAI;
         bool isInitialized = false;
 
+        public float Timer = 40f;
+        float privateTimer;
+        float StopTimer = 10f;
+
+        private float _stopTimer;
+
+        bool canToggle
+        {
+            get
+            {
+                if (privateTimer <= 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        bool isToggled { get { return !dumbyAI.IsActive; } }
+
         public void Init()
         {
             dumbyAI = GameManager.I.Dumby as Dumby;
-
+            privateTimer = Timer;
+            _stopTimer = StopTimer;
             isInitialized = true;
         }
 
-        public void ToggleAI()
+        private void Update()
         {
             if (!isInitialized)
                 Init();
 
+            privateTimer -= Time.deltaTime;
+            if (isToggled)
+                _stopTimer -= Time.deltaTime; 
+
+            if(_stopTimer <= 0)
+            {
+                dumbyAI.IsActive = true;
+                privateTimer = Timer - _stopTimer;
+                _stopTimer = StopTimer;
+            }
+        }
+
+        public void ToggleAI()
+        {
+            if (!canToggle || isToggled)
+                return;
+
             dumbyAI.IsActive = !dumbyAI.IsActive;
+            privateTimer = Timer;
+            _stopTimer = StopTimer;
         }
     }
 }
