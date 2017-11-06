3bdf3f33bd669011304f4508d2175423e97dff9e
Luca Garbelli <lu.gbelli@hotmail.it>
2017-11-06 16:32:31 +0100

    - corrette rocce

--- Assets/Scripts/Rooms/Floor.cs
+++ Assets/Scripts/Rooms/Floor.cs
@@ -12,7 +12,7 @@ namespace DumbProject.Rooms
             base.SetFillerGraphic(_graphic, _position, _rotation);
             if(fillerGraphics[0] != null)
             {
-                fillerGraphics[0].transform.position += new Vector3(Random.Range(0.1f, 1.8f), 0f, Random.Range(0.1f, 1.8f));
+                fillerGraphics[0].transform.position += new Vector3(0f, Random.Range(0.01f, 0.1f), 0f);
                 fillerGraphics[0].transform.localScale = new Vector3(Random.Range(0.8f, 1.3f), Random.Range(0.8f, 1.3f), Random.Range(0.8f, 1.3f));
                 fillerGraphics[0].transform.localEulerAngles = new Vector3(0f, Random.Range(0f, 360f), 0f);
 
